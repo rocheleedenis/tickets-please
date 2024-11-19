4381d127e9a848d2787745eaeaf1fe2e782f5b59
Rochele <rochele.edenis@gmail.com>
2024-11-18 21:46:54 -0300

    feat: add UsersController and user resource

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketsController;
+use App\Http\Controllers\Api\V1\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->apiResource('/tickets', TicketsController::class);
+Route::middleware('auth:sanctum')->apiResource('/users', UsersController::class);
