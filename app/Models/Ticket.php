a9b62c8ea5f66ee7b05477c875c6295276cc79d6
Rochele <rochele.edenis@gmail.com>
2024-11-20 20:09:27 -0300

    refactor: simplify user relationship method in Ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
     /** @use HasFactory<\Database\Factories\TicketsFactory> */
     use HasFactory;
 
-    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
